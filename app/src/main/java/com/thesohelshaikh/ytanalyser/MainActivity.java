9152f6105ab5b854c5099bdbe240e0d3e151271a
Sohel Shaikh <sohelshaikh97@gmail.com>
2020-12-26 22:59:33 +0530

    Add volley and get api working

--- app/src/main/java/com/thesohelshaikh/ytanalyser/MainActivity.java
+++ app/src/main/java/com/thesohelshaikh/ytanalyser/MainActivity.java
@@ -8,6 +8,18 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     private final String API_KEY = "AIzaSyDR6pW44r1itzKNBJg3U0mXOkbZCoXhkhE";
     private final String code = "-QMg39gK624";
@@ -25,7 +37,29 @@ public class MainActivity extends AppCompatActivity {
         btnAnalyse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), edURL.getText(), Toast.LENGTH_SHORT).show();
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=fis26HvvDII&key=AIzaSyBJ-YGFNIDYeGo5pTvexZ2fJgsM6Erenkk";
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String duration = response.getJSONArray("items").getJSONObject(0).getJSONObject("contentDetails").getString("duration");
+                            Toast.makeText(MainActivity.this, duration, Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(jsonObjectRequest);
+
             }
         });
     }
