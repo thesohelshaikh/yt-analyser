d5f483b0b04c00fb74c7c0df654d748caff8f76c
Sohel Shaikh <sohelshaikh97@gmail.com>
2023-07-16 01:06:10 +0530

    Add ability to get url from clipboard

--- app/src/main/java/com/thesohelshaikh/ytanalyser/ui/home/HomeContent.kt
+++ app/src/main/java/com/thesohelshaikh/ytanalyser/ui/home/HomeContent.kt
@@ -1,5 +1,8 @@
 package com.thesohelshaikh.ytanalyser.ui.home
 
+import android.content.ClipboardManager
+import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -16,16 +19,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.thesohelshaikh.ytanalyser.R
 import com.thesohelshaikh.ytanalyser.UtilitiesManger
 
@@ -51,6 +60,12 @@ fun HomeContent(
         )
 
         var urlInput by remember { mutableStateOf("") }
+        val context = LocalContext.current
+
+        ListenLifecycleEvents {
+            if (urlInput.isNotEmpty()) return@ListenLifecycleEvents
+            urlInput = getStringFromClipboard(context)
+        }
 
         OutlinedTextField(
             value = urlInput,
@@ -74,7 +89,7 @@ fun HomeContent(
                     val videoId = validateUrl(urlInput) ?: return@KeyboardActions
                     onClickAnalyse(videoId)
                 }
-            )
+            ),
         )
 
         Button(
@@ -91,7 +106,40 @@ fun HomeContent(
     }
 }
 
+@Composable
+private fun ListenLifecycleEvents(onResume: () -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val currentOnResume by rememberUpdatedState(onResume)
+
+    DisposableEffect(lifecycleOwner) {
+        // Create an observer that triggers our remembered callbacks
+        // for sending analytics events
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
+                currentOnResume()
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
+
 private fun validateUrl(urlInput: String): String? {
     if (urlInput.isBlank() || urlInput.isEmpty()) return null
     return UtilitiesManger.getIDfromURL(urlInput)
+}
+
+private fun getStringFromClipboard(context: Context): String {
+    val clipboardManager =
+        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clipText = clipboardManager.primaryClip?.getItemAt(0)?.text ?: ""
+    Log.d("TAG", "getStringFromClipboard: $clipText, ${clipboardManager.primaryClip}")
+    if (clipText.startsWith("http")) return clipText.toString()
+    return ""
 }
