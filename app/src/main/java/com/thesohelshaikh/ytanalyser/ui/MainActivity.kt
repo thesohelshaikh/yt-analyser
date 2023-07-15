caa26ed67ca336c3687b6710f6b6d7b22625b8a0
Sohel Shaikh <sohelshaikh97@gmail.com>
2023-07-15 19:33:08 +0530

    Bump dependencies

--- app/src/main/java/com/thesohelshaikh/ytanalyser/ui/MainActivity.kt
+++ app/src/main/java/com/thesohelshaikh/ytanalyser/ui/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.thesohelshaikh.ytanalyser.ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import com.thesohelshaikh.ytanalyser.ui.home.MyApp
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
