f68318e3eadc46bb4cd971ca3f4523f24acd2160
Sohel Shaikh <sohelshaikh97@gmail.com>
2023-07-15 19:24:35 +0530

    Remove unused libraries

--- app/src/main/java/com/thesohelshaikh/ytanalyser/data/local/TypeConverters.kt
+++ app/src/main/java/com/thesohelshaikh/ytanalyser/data/local/TypeConverters.kt
@@ -1,17 +1,14 @@
 package com.thesohelshaikh.ytanalyser.data.local
 
 import androidx.room.TypeConverter
-import com.google.common.reflect.TypeToken
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.lang.reflect.Type
 
 
 object RoomTypeConverters {
     @TypeConverter
     fun fromString(value: String): List<String?>? {
-        val listType: Type = object : TypeToken<List<String?>?>() {}.type
         return Json.decodeFromString(value)
     }
 
