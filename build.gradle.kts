ec8dc9016405e9113172b893eb3c2b52b650d31d
Sohel Shaikh <sohelshaikh97@gmail.com>
2023-08-23 00:09:50 +0530

    Bump AGP to 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val kotlinVersion = "1.8.10"
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
 
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
