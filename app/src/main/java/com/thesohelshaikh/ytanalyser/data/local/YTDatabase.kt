48a30ef391a25ff124e1a3108a7971ad900b14f8
Sohel Shaikh <sohelshaikh97@gmail.com>
2024-07-06 15:44:28 +0530

    Remove unused method

--- app/src/main/java/com/thesohelshaikh/ytanalyser/data/local/YTDatabase.kt
+++ app/src/main/java/com/thesohelshaikh/ytanalyser/data/local/YTDatabase.kt
@@ -1,8 +1,6 @@
 package com.thesohelshaikh.ytanalyser.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.thesohelshaikh.ytanalyser.data.local.dao.PlaylistDao
@@ -21,27 +19,4 @@ abstract class YTDatabase : RoomDatabase() {
     abstract fun videoDao(): VideoDao
 
     abstract fun playlistDao(): PlaylistDao
-
-    companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
-        @Volatile
-        private var INSTANCE: YTDatabase? = null
-
-        fun getDatabase(context: Context): YTDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        YTDatabase::class.java,
-                        "yt_database"
-                ).fallbackToDestructiveMigration()
-                        .build()
-                INSTANCE = instance
-                // return instance
-                instance
-            }
-        }
-    }
 }
