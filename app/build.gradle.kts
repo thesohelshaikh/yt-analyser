c9331264a81492cd78c9d6fc9114340857a0893c
Sohel Shaikh <sohelshaikh97@gmail.com>
2024-07-27 16:48:02 +0530

    Update test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,10 +99,10 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class-android:1.2.1")
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
